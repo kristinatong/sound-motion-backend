23bbb5f08c6bd41f412a74421fe2641bd583760a
kristinatong <kristina.tong@gmail.com>
2018-11-07 14:51:11 -0500

    added active storage functionality

---
+++ app/controllers/api/v1/sounds_controller.rb
@@ -0,0 +1,37 @@
+class Api::V1::SoundsController < ApplicationController
+  before_action :set_storage_url
+  skip_before_action :authorized
+
+  def index
+    @sounds = Sound.all
+    render json: @sounds
+  end
+
+  def create
+    @sound = Sound.create(sound_params)
+    @sound.file.attach(sound_params[:file])
+    render json: {
+      name: @sound.name,
+      url: url_for(@sound.file)
+    }
+  end
+
+  def show
+    @sound = Sound.find(params[:id])
+    render json: {
+      name: @sound.name,
+      url: @sound.file.service_url
+    }
+  end
+
+  private
+
+  def sound_params
+    params.permit(:name, :file)
+  end
+
+  def set_storage_url
+    ActiveStorage::Current.host = request.base_url
+  end
+
+end
---
+++ db/migrate/20181107154608_create_sounds.rb
@@ -0,0 +1,9 @@
+class CreateSounds < ActiveRecord::Migration[5.2]
+  def change
+    create_table :sounds do |t|
+      t.string :name
+
+      t.timestamps
+    end
+  end
+end
